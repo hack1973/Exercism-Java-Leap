32b4d6e1d7962b7569204b4a6c1083614ab0a684
Michael Hacker <hack1973@swbell.net>
2019-11-22 13:01:15 -0600

    completed code for exercise/tests pass

--- src/main/java/Leap.java
+++ src/main/java/Leap.java
@@ -1,7 +1,16 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        boolean leapYear = false;
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            leapYear = true;
+        } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            leapYear = true;
+        } else {
+            leapYear = false;
+        }
+        return leapYear;
     }
 
 }
